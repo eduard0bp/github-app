7bd78ca7517abbff0210f07f9ad695d9e518c61c
Eduardo Batista Pereira <duuhpwn@gmail.com>
2022-09-29 21:59:57 -0300

    Marcação do App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,48 @@
 
 import React from "react";
 
-const App = () => <div>My App</div>;
+const App = () => (
+  <div className="app">
+    <div className="search">
+      <input type="search" placeholder="Digite o nome do usuário no GitHub" />
+    </div>
+
+    <div className="user-info">
+      <img src="https://avatars.githubusercontent.com/u/79476975?v=4" />
+      <h1>
+        <a href="https://github.com/eduard0bp">Eduardo Batista</a>
+      </h1>
+
+      <ul className="repos-info">
+        <li>- Repositórios: 16</li>
+        <li>- Seguidores: 03</li>
+        <li>- Seguindo: 03</li>
+      </ul>
+
+      <div className="actions">
+        <button>Ver repositórios</button>
+        <button>Ver favoritos</button>
+      </div>
+
+      <div className="repos">
+        <h2>Repositórios:</h2>
+        <ul>
+          <li>
+            <a href="#">Nome do repositório</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="starred">
+        <h2>Favoritos:</h2>
+        <ul>
+          <li>
+            <a href="#">Nome do repositório</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
